57c87ed940099c4a6e407517adce799bad70eb8a
Aether926 <warmsteve122@gmail.com>
2025-01-03 21:23:58 +0800

    init navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='bg-indigo-700'>
+        <p>hutaosupremacy</p>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
