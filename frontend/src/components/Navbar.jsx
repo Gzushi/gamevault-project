b881639abdb6651e6e84ff3e132fb1be22627725
Aether926 <warmsteve122@gmail.com>
2025-01-04 00:15:35 +0800

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <>
-      <nav className='bg-white'>
-        <p>hutaosupremacy</p>
-      </nav>
-    </>
+      <div>
+        <nav className='bg-[#2E2E2E] p-6 flex justify-between'>
+          <div className='flex justify-between w-96'>
+            <div>
+              <h1 className='text-white p-1.5'>GameVault</h1>
+            </div>
+            <div>
+              <input className='bg-[#4D4D4D] flex p-1 pl-4 pr-20 border-2 border-[#4D4D4D] outline-none rounded-md' type="text" placeholder='Search'/>
+            </div>
+          </div>
+          <div>
+            <h1 className='text-white flex p-1.5'>Become a Dev!</h1>
+          </div>
+        </nav>
+      </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './pages/App'
+import LogIn from './pages/LogIn'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+  {
+    path: '/login',
+    element: <LogIn/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
