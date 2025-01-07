7e01aa689cc479e36d26f357dc83bef7fd4f5062
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-01-07 23:07:26 +0800

    reformated login and user forms

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,57 +13,57 @@ import LogInDev from './pages/LogInDev'
 import LogInAdmin from './pages/LogInAdmin'
 import SignUpDev from './pages/SignUpDev'
 import SignUpAdmin from './pages/SignUpAdmin'
-import { AuthContextProvider } from './context/AuthContext'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/login',
-    element: <LogIn />,
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-  },
-  {
-    path: '/addgames',
-    element: <AddGames />,
-  },
-  {
-    path: '/gameprofile',
-    element: <GameProfile />,
-  },
-  {
-    path: '/gameprofile/:id',
-    element: <GameProfile />,
-  },
-  {
-    path: '/userprofile',
-    element: <UserProfile />,
-  },
-  {
-    path: '/devprofile',
-    element: <DevProfile />,
-  },
-  {
-    path: '/logindev',
-    element: <LogInDev />,
-  },
-  {
-    path: '/loginadmin',
-    element: <LogInAdmin />,
-  },
-  {
-    path: '/signupdev',
-    element: <SignUpDev />,
-  },
-  {
-    path: '/signupAdmin',
-    element: <SignUpAdmin />,
-  },
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/login',
+        element: <LogIn />,
+    },
+    {
+        path: '/signup',
+        element: <SignUp />,
+    },
+    {
+        path: '/addgames',
+        element: <AddGames />,
+    },
+    {
+        path: '/gameprofile',
+        element: <GameProfile />,
+    },
+    {
+        path: '/gameprofile/:id',
+        element: <GameProfile />,
+    },
+    {
+        path: '/userprofile',
+        element: <UserProfile />,
+    },
+    {
+        path: '/devprofile',
+        element: <DevProfile />,
+    },
+    {
+        path: '/logindev',
+        element: <LogInDev />,
+    },
+    {
+        path: '/loginadmin',
+        element: <LogInAdmin />,
+    },
+    {
+        path: '/signupdev',
+        element: <SignUpDev />,
+    },
+    {
+        path: '/signupAdmin',
+        element: <SignUpAdmin />,
+    },
 ])
 
 createRoot(document.getElementById('root')).render(
