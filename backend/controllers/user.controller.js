5d991b84a0d6bfa6da9df81fd4770f2ca784ccf4
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-01-07 22:27:38 +0800

    login auth and token complete

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,30 @@
-import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+    return jwt.sign({ _id: id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+export const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.login(username, password);
+        const token = createToken(user._id);
+        res.status(200).json({ username, token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
 
 export const signupUser = async (req, res) => {
     const { username, password, email } = req.body;
 
     try {
         const user = await User.signup(username, password, email);
-        res.status(200).json({ username, user });
+        const token = createToken(user._id);
+        res.status(200).json({ username, token });
     } catch (error) {
-        res.status(400).json({ error: error.message, else: "this" });
+        res.status(400).json({ error: error.message });
     }
 };
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const UserSchema = new mongoose.Schema(
     {
         username: {
             type: String,
             required: true,
+            unique: true,
         },
         password: {
             type: String,
@@ -14,7 +16,6 @@ const UserSchema = new mongoose.Schema(
         email: {
             type: String,
             required: true,
-            unique: true,
         },
         role: {
             type: String,
@@ -27,11 +28,43 @@ const UserSchema = new mongoose.Schema(
     }
 );
 
-UserSchema.statics.signup = async function (username, password, email) {
-    const exist = await this.findOne({ email });
+UserSchema.statics.login = async function (username, password) {
+    if (!username || !password) {
+        throw Error("All fields must be filled");
+    }
 
-    if (exist) {
-        throw Error("Email already exist");
+    const user = await this.findOne({ username });
+
+    if (!user) {
+        throw Error("Incorrect username");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error("Incorrect password");
+    }
+
+    return user;
+};
+
+UserSchema.statics.signup = async function (username, password, email) {
+    if (!username || !password || !email) {
+        throw Error("All fields must be filled");
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Must be a valid Email");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password not strong enough");
+    }
+
+    const existUser = await this.findOne({ username });
+
+    if (existUser) {
+        throw Error("User already exist");
     }
 
     const salt = await bcrypt.genSalt(10);
