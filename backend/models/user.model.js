b036a6f6083e61f383aacfcef9d1a8b563b3f602
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-01-07 23:08:45 +0800

    added admin role

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema(
         },
         role: {
             type: String,
-            enum: ["user", "dev"],
+            enum: ["user", "dev", "admin"],
             default: "user",
         },
     },
