ab928cad987cab83455bc72da359fcb1e24271f9
Gzushi <johnsenecitomat18@gmail.com>
2025-01-08 10:06:48 +0800

    Laetstsw

--- frontend/src/components/UserOrDevMetrics.jsx
+++ frontend/src/components/UserOrDevMetrics.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const UserOrDevMetrics = () => {
+const { user } = useAuthContext()
   return (
     <div className='bg-[#212121] flex flex-row flex-fit items-center gap-5 px-4 py-2 mb-6 w-min rounded-2xl'>
         <div className='flex flex-col items-center'>
-            <p>##</p>
+            <p>{user && user.dislikes}</p>
             <p>Following</p>
         </div>
         <div className='flex flex-col items-center'>
@@ -12,7 +14,7 @@ const UserOrDevMetrics = () => {
             <p>Followers</p>
         </div>
         <div className='flex flex-col items-center'>
-            <p>##</p>
+            <p>{user && user.username}</p>
             <p>Likes</p>
         </div>
         <div className='flex flex-col items-center'>
